994826618e5477a4f352c537dda2103e92092db2
Dasari <dasarimanoj265@gmail.com>
2024-08-03 12:07:51 +0530

    updated spl-metadata and spl- transfer

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,8 +1,9 @@
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/wba-wallet.json";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
 import { readFile } from "fs/promises"
+import { read } from "fs"
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -16,13 +17,15 @@ umi.use(signerIdentity(signer));
 (async () => {
     try {
         //1. Load image
+        const images = await readFile('D:\manoj-projects\Turbin3-portfolio\ts\cluster1\images\generug.png');
         //2. Convert image to generic file.
+        const generic = createGenericFile(images, "generug.png", { contentType: "image/png"});
         //3. Upload image
-
+        const [uri] = await umi.uploader.upload([generic])
         // const image = ???
-
+        
         // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
+        console.log("Your image URI: ", uri);
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { 
     createMetadataAccountV3, 
@@ -7,9 +7,10 @@ import {
     DataV2Args
 } from "@metaplex-foundation/mpl-token-metadata";
 import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import bs58 from "bs58";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("75hqagix9zAXEy9CewhcxjYdXmy6Ta16EzWip1bAxcHy");
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -19,29 +20,37 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
 (async () => {
     try {
-        // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint, 
+            mintAuthority:signer
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: "resiquents",
+            symbol: "hehe",
+            uri: "",
+            sellerFeeBasisPoints: 0,
+            creators: null,
+            collection: null,
+            uses: null
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data: data,
+            isMutable: true,
+            collectionDetails: null
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
+        let result = await tx.sendAndConfirm(umi);
+        console.log(bs58.encode(result.signature));
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
