1282140d11f30f646524807f7353aa22b11c96b0
Manoj Dasari <dasarimanoj265@gmail.com>
2024-09-11 15:19:30 +0530

    nft-staking done

---
+++ nft-staking/programs/nft-staking/src/errors.rs
@@ -0,0 +1,11 @@
+use anchor_lang::error_code;
+
+#[error_code]
+pub enum StakeError {
+    #[msg("Frezze period not passed")]
+    FreezePeriodNotPassed,
+    #[msg("Max stake reached")]
+    MaxStakeReached,
+    #[msg("Stake is not matured")]
+    StakeNotMatured,
+}
