cef35a39efd64d101ee6d17ae1586c86e9a2d1d1
Manoj Dasari <dasarimanoj265@gmail.com>
2024-10-03 23:43:33 +0530

    configured bet parameters

--- capstone-project/scripts/config/99-config.ts
+++ capstone-project/scripts/config/99-config.ts
@@ -30,8 +30,20 @@ export const solUsdSwitchboardFeedDevnet = "8g6zZtZFLJCRBm85rZbMws3ce2oqzzDKEGBj
 
 const programId = new PublicKey("E5kv2j41SfsrZyCeEohk8SQ3i71Yzgiv32ey8ekeL5mQ");
 
-//TODO - init program
+//init program
+export const initSeed = new BN(123); // vary this to create multiple instances from the same admin wallet
+export const fees = 1000; // 1,000 = 10%
 
-//TODO - bet program
+//bet parameters
+export const betSeed = new BN(999); // vary this to have multiple open bets from the same creator wallet open at the same time
+export const currentTime = new BN(Date.now() / 1000); // Current unix timestamp in seconds
+export const deadlineToJoin = new BN(currentTime.toNumber() + 30); // 30 seconds to join
+export const startTime = new BN(currentTime.toNumber() + 35); // Bet starts after 35 seconds
+export const endTime = new BN(currentTime.toNumber() + 65); // Bet ends after 65 seconds
+export const pricePrediction = new BN(2000 * (10**8)); // Price prediction with 8 decimal places
+export const wagerAmount = new BN(0.1 * web3.LAMPORTS_PER_SOL); // 0.1 SOL wager
+export const makerOdds = new BN(60); // 60% odds for maker
+export const opponentOdds = new BN(40); // 40% odds for opponent
+export const creatorEstimate = true; // Maker predicts price will be higher
 
 //TODO - account PDAs
